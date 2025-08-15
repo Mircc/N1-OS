014c85fcf493a4f40f6df43349b1a394e1690f88
Mircc <Mircc@users.noreply.github.com>
2025-08-15 02:47:07 +0000

    Update diy-Imm.sh
    更新passwall相关插件

--- N1-IMM/diy-Imm.sh
+++ N1-IMM/diy-Imm.sh
@@ -12,5 +12,16 @@ git clone https://github.com/vernesong/OpenClash --depth=1 clone/openclash
 rm -rf feeds/luci/applications/luci-app-openclash
 cp -rf clone/openclash/luci-app-openclash feeds/luci/applications/
 
+#以下为passwall相关
+# 移除 openwrt feeds 自带的核心库
+rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
+git clone https://github.com/xiaorouji/openwrt-passwall-packages package/passwall-packages
+
+# 移除 openwrt feeds 过时的luci版本
+rm -rf feeds/luci/applications/luci-app-passwall
+git clone https://github.com/xiaorouji/openwrt-passwall package/passwall-luci
+
+#passwall相关结束
+
 # Clean packages
 rm -rf clone
