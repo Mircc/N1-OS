59fed226a5a24aa4a191b86efec904a5ba183b63
Mircc <Mircc@users.noreply.github.com>
2025-08-15 06:49:02 +0000

    Update diy-beta.sh

--- N1-LEDE/diy-beta.sh
+++ N1-LEDE/diy-beta.sh
@@ -46,7 +46,7 @@ rm -rf feeds/luci/applications/luci-app-mosdns
 
 # ==================== 基础系统设置 ====================
 # 默认 IP
-sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.50.200/g' package/base-files/files/bin/config_generate
 
 # 修改默认时间格式
 sed -i 's/os.date()/os.date("%Y-%m-%d %H:%M:%S %A")/g' $(find ./package/*/autocore/files/ -type f -name "index.htm")
